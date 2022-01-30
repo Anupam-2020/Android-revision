7fc6d2be6f85bef456ccf9d6fcfa4fd6a846784d
aanand081 <anupamanand.696@gmail.com>
2022-01-30 12:33:06 +0530

    Coroutines

--- Day 3/app/src/main/java/com/dfarm/viewmodelactivity/SecondFragment.kt
+++ Day 3/app/src/main/java/com/dfarm/viewmodelactivity/SecondFragment.kt
@@ -1,12 +1,14 @@
 package com.dfarm.viewmodelactivity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.dfarm.viewmodelactivity.databinding.FragmentSecondBinding
+import kotlinx.coroutines.*
 
 
 class SecondFragment : Fragment() {
@@ -31,6 +33,48 @@ class SecondFragment : Fragment() {
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        binding.counterBtn.setOnClickListener {
+            binding.textviewSecond.text = "${binding.textviewSecond.text.toString().toInt()+1}"
+            Log.i("Thread","${Thread.currentThread().name}")
+        }
+        
+        binding.longRunning.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                executeLongRunning()
+                Log.i("@Back","1 - ${Thread.currentThread().name}")
+            }
+
+            GlobalScope.launch(Dispatchers.IO) {
+                executeLongRunning()
+                Log.i("@Back","2 - ${Thread.currentThread().name}")
+            }
+
+            CoroutineScope(Dispatchers.IO).launch {
+                task1()
+            }
+            CoroutineScope(Dispatchers.IO).launch {
+                task2()
+            }
+        }
+    }
+
+    suspend fun task1() {
+        Log.i("TAG","STARTING TASK 1")
+        delay(2000)
+        Log.i("TAG","ENDING TASK 1")
+    }
+
+    suspend fun task2() {
+        Log.i("TAG","STARTING TASK 2")
+        delay(2000)
+        Log.i("TAG","ENDING TASK 2")
+    }
+
+    fun executeLongRunning() {
+        for (i in 1..1000000000L) {
+
         }
     }
 
